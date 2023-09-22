60e129bbc2af250e23881267ab946e31690cea41
Oguz Han Asnaz <github@lostgeek.de>
2023-09-22 21:16:52 +0200

    Justify content in Expansion Panel

--- components/content/ExpansionPanel.vue
+++ components/content/ExpansionPanel.vue
@@ -3,8 +3,8 @@
         :class="mdAndUp ? 'w-75' : 'w-100'"
         color="primary">
         <v-expansion-panel :title="props.title">
-            <v-expansion-panel-text>
-                <ContentSlot :use="$slots.default" unwrap="p" />
+            <v-expansion-panel-text class="text-justify">
+                <ContentSlot :use="$slots.default" />
             </v-expansion-panel-text>
         </v-expansion-panel>
     </v-expansion-panels>
--- content/about.md
+++ content/about.md
@@ -1,3 +1,4 @@
 # About
 Project CROW is a series of Netrunner campaign decks made by lostgeek.
-Find the GitHub repository for the website [here](https://github.com/lostgeek/project-crow))!
+
+You can find the GitHub repository for the website [here](https://github.com/lostgeek/project-crow))!
