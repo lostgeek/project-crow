33898b228b710a208af3f3da0bd59174e7927997
Oguz Han Asnaz <github@lostgeek.de>
2023-09-23 00:25:06 +0200

    Retab nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,79 +4,79 @@ import colors from 'vuetify/lib/util/colors'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  modules: [
-    '@nuxt/content',
-    '@invictus.codes/nuxt-vuetify',
-    '@vueuse/nuxt',
-    ['@nuxtjs/google-fonts', {
-      families: {
-        'Josefin+Sans': true,
-        Lato: true,
-      },
-      preload: true,
-    }],
-  ],
-  vuetify: {
-    vuetifyOptions: {
-      display: {
-        mobileBreakpoint: 'sm',
-      },
-      icons: {
-        defaultSet: 'mdi',
-        aliases,
-        sets: {
-          mdi,
-        },
-      },
-      theme: {
-        defaultTheme: 'dark',
-        themes: {
-          light: {
-            dark: false,
-            colors: {
-              primary: colors.amber.darken2,
-              primarytext: colors.amber.darken3,
-              secondary: colors.blueGrey.darken2,
-              secondarytext: colors.blueGrey.darken3,
-              secondarybg: colors.blueGrey.lighten5,
-              link: colors.amber.base,
+    devtools: { enabled: false },
+    modules: [
+        '@nuxt/content',
+        '@invictus.codes/nuxt-vuetify',
+        '@vueuse/nuxt',
+        ['@nuxtjs/google-fonts', {
+            families: {
+                'Josefin+Sans': true,
+                Lato: true,
             },
-          },
-          dark: {
-            dark: true,
-            colors: {
-              primary: colors.amber.lighten1,
-              primarytext: colors.amber.lighten1,
-              secondary: colors.blueGrey.lighten1,
-              secondarytext: colors.blueGrey.lighten2,
-              secondarybg: colors.blueGrey.darken4,
-              link: colors.amber.darken2,
-              background: '#212121',
+            preload: true,
+        }],
+    ],
+    vuetify: {
+        vuetifyOptions: {
+            display: {
+                mobileBreakpoint: 'sm',
             },
-          },
+            icons: {
+                defaultSet: 'mdi',
+                aliases,
+                sets: {
+                    mdi,
+                },
+            },
+            theme: {
+                defaultTheme: 'dark',
+                themes: {
+                    light: {
+                        dark: false,
+                        colors: {
+                            primary: colors.amber.darken2,
+                            primarytext: colors.amber.darken3,
+                            secondary: colors.blueGrey.darken2,
+                            secondarytext: colors.blueGrey.darken3,
+                            secondarybg: colors.blueGrey.lighten5,
+                            link: colors.amber.base,
+                        },
+                    },
+                    dark: {
+                        dark: true,
+                        colors: {
+                            primary: colors.amber.lighten1,
+                            primarytext: colors.amber.lighten1,
+                            secondary: colors.blueGrey.lighten1,
+                            secondarytext: colors.blueGrey.lighten2,
+                            secondarybg: colors.blueGrey.darken4,
+                            link: colors.amber.darken2,
+                            background: '#212121',
+                        },
+                    },
+                },
+            },
         },
-      },
+
+        moduleOptions: {
+            /* nuxt-vuetify module options */
+            treeshaking: true,
+            useIconCDN: false,
+
+            /* vite-plugin-vuetify options */
+            styles: 'sass',
+            autoImport: true,
+            useVuetifyLabs: true,
+        }
     },
-
-    moduleOptions: {
-      /* nuxt-vuetify module options */
-      treeshaking: true,
-      useIconCDN: false,
-
-      /* vite-plugin-vuetify options */
-      styles: 'sass',
-      autoImport: true,
-      useVuetifyLabs: true,
-    }
-  },
-  content: {
-    documentDriven: true,
-    contentHead: false,
-  },
-  css: [
-    // SCSS file in the project
-    "~/assets/style/main.scss",
-    '@mdi/font/css/materialdesignicons.css',
-  ],
+    content: {
+        documentDriven: true,
+        contentHead: false,
+    },
+    css: [
+        // SCSS file in the project
+        "~/assets/style/main.scss",
+        '@mdi/font/css/materialdesignicons.css',
+    ],
 })
